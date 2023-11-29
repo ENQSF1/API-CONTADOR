f435aae45eeaf0e0c0cb507bbf6354eed7f91b14
kevin hernandez <kevinhernandz15@gmail.com>
2023-11-28 23:31:08 -0600

    cors

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ const express = require("express");
 const app = express();
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 app.set("port", process.env.PORT || 3000);
 
 app.use(morgan("dev"));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(bodyParser.json());
+app.use(cors());
 
 let jsonData = [];
 
